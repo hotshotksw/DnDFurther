7ca46bdbe50f359f78b777a66b2489a6ef69c374
Elijah Kelting <elijahakbusiness@gmail.com>
2024-02-20 10:53:05 -0700

    Update Character.cs
    added the main stats

--- Models/Character.cs
+++ Models/Character.cs
@@ -40,13 +40,31 @@ namespace DnD_Further.Models
         public string? Name { get; set; }
         [Required]
         public Races Race { get; set; }
+        [Required, Range(1, 20)]
+        public int Strength { get; set; }
+        [Required, Range(1, 20)]
+        public int Dexterity { get; set; }
+        [Required, Range(1, 20)]
+        public int Constitution { get; set; }
+        [Required, Range(1, 20)]
+        public int Intelligence { get; set; }
+        [Required, Range(1, 20)]
+        public int Wisdom { get; set; }
+        [Required, Range(1, 20)]
+        public int Charisma { get; set; }
 
         public Character() { }
 
-        public Character(string name, Races race)
+        public Character(string name, Races race, int str, int dex, int con, int intl, int wis, int cha)
         {
             this.Name = name;
             this.Race = race;
+            this.Strength = str;
+            this.Dexterity = dex;
+            this.Constitution = con;
+            this.Intelligence = intl;
+            this.Wisdom = wis;
+            this.Charisma = cha;
         }
     }
 }
