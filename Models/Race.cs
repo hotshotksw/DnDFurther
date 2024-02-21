a27d0cb57e163f825d79b4b4f461b58b754864dd
Elijah Kelting <elijahakbusiness@gmail.com>
2024-02-21 11:39:45 -0700

    started database correlation
    correlation for class and race started

---
+++ Models/Race.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DnD_Further.Models
+{
+    public class Race
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required, MaxLength(100)]
+        public string Title { get; set; }
+    }
+}
