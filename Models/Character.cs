5f0317217754d6590f75d388109c70c043f9e7a1
Elijah Kelting <elijahakbusiness@gmail.com>
2024-02-21 11:55:11 -0700

    Revert "started database correlation"
    This reverts commit a27d0cb57e163f825d79b4b4f461b58b754864dd.

--- Models/Character.cs
+++ Models/Character.cs
@@ -4,18 +4,46 @@ namespace DnD_Further.Models
 {
     public class Character
     {
+        public enum Races
+        {
+            Dragonborn,
+            Dwarf,
+            Elf,
+            Gnome,
+            Half_Elf,
+            Half_Orc,
+            Halfing,
+            Human,
+            Tiefling
+        }
+
+        public enum Classes
+        {
+            Barbarian,
+            Bard,
+            Cleric,
+            Druid,
+            Fighter,
+            Monk,
+            Paladin,
+            Ranger,
+            Rogue,
+            Sorcerer,
+            Warlock,
+            Wizard
+        }
+
+
         [Key]
         public int Id { get; set; }
         [MaxLength(450)]
         public string? UserID { get; set; }
         [Required]
         public string? Name { get; set; }
-        
-        public int? ClassID { get; set; }
-        public Class? Class { get; set; }
-        public int? RaceID { get; set; }
-        public Race? Race { get; set; }
-
+        [Required, EnumDataType(typeof(Races))]
+        public Races RaceType { get; set; }
+        [Required, EnumDataType(typeof(Classes))]
+        public Classes ClassType { get; set; }
         [Required, Range(1, 20)]
         public int Strength { get; set; }
         [Required, Range(1, 20)]
@@ -31,11 +59,11 @@ namespace DnD_Further.Models
 
         public Character() { }
 
-        public Character(string name, Race race, Class classes, int str, int dex, int con, int intl, int wis, int cha)
+        public Character(string name, Races race, Classes classes, int str, int dex, int con, int intl, int wis, int cha)
         {
             this.Name = name;
-            this.Race = race;
-            this.Class = classes;
+            this.RaceType = race;
+            this.ClassType = classes;
             this.Strength = str;
             this.Dexterity = dex;
             this.Constitution = con;
